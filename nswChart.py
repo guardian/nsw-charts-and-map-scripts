0205f9c1e29d6f4020c34f48b30ccfaec9b2e37f
nickjevershed <nick.evershed@gmail.com>
2021-08-12 13:33:33 +1000

    LGA charts

--- nswChart.py
+++ nswChart.py
@@ -87,7 +87,7 @@ df3 = df3[~df3.index.duplicated()]
 lastUpdated2 = df3.index[-1]
 # newUpdated = lastUpdated2
 newUpdated = lastUpdated2 + timedelta(days=1)
-newUpdated = newUpdated.strftime('%-d %B, %Y')
+newUpdated = newUpdated.strftime('%-d %B %Y')
 
 df4 = df3.copy()
 
@@ -158,7 +158,7 @@ def makeLocalLine(df):
 	template = [
 			{
 				"title": "Trend in local and overseas-related transmission of Covid-19 in NSW, last 60 days",
-				"subtitle": "Showing the 7 day rolling average of locally and overseas-acquired cases, with those under investigation added to the local category. Last updated {date}".format(date=newUpdated),
+				"subtitle": "Showing the seven-day rolling average of locally and overseas-acquired cases, with those under investigation added to the local category. Last updated {date}".format(date=newUpdated),
 				"footnote": "",
 				"source": "NSW Health",
 				"dateFormat": "%Y-%m-%d",
@@ -209,6 +209,7 @@ short = local_gp[thirty_days:]
 
 short_p = short.pivot(columns='lga_name19', values='count')
 
+
 map_index = pd.date_range(start=thirty_days, end=lastUpdated)
 short_p = short_p.reindex(map_index)
 
@@ -224,11 +225,13 @@ recent_totals = short_p.sum()
 totals = totals[totals >= 5]
 
 short_p.fillna(0, inplace=True)
+
 no_negs = short_p
 
 no_negs[short_p < 0] = 0
 
 rolling = no_negs.rolling(7).mean()
+
 rolling.to_csv("rolling.csv")
 lga_movement = []
 
@@ -255,6 +258,93 @@ recent_totals.to_json()
 
 #%%
 
+
+
 syncData(lga_movement,'2020/07/nsw-corona-map{test}'.format(test=test), "nswChange")
 
 syncData(recent_totals.to_dict('records'),'2020/07/nsw-corona-map{test}'.format(test=test), "recentLocal")
+
+#%%
+long = local_gp["2021-06-10":]
+long_p = long.pivot(columns='lga_name19', values='count')
+long_p.fillna(0, inplace=True)
+long_p[short_p < 0] = 0
+long_p_rolling = long_p.rolling(7).mean()
+
+lga_totals = lga_df_movement1.copy()
+lga_totals = lga_totals.sort_values(['this_week'], ascending=False)
+
+
+# shortlist = [
+# "Bayside (A)",
+# "Blacktown (C)",
+# "Burwood (A)",
+# "Camden (A)",
+# "Campbelltown (C) (NSW)",
+# "Canada Bay (A)",
+# "Canterbury-Bankstown (A)",
+# "Central Coast (C) (NSW)",
+# "Cumberland (A)",
+# "Fairfield (C)",
+# "Georges River (A)",
+# "Inner West (A)",
+# "Liverpool (C)",
+# "Maitland (C)",
+# "Newcastle (C)",
+# "Northern Beaches (A)",
+# "Parramatta (C)",
+# "Penrith (C)",
+# "Randwick (C)",
+# "Ryde (C)",
+# "Strathfield (A)",
+# "Sutherland Shire (A)",
+# "Sydney (C)",
+# "The Hills Shire (A)",
+# "Waverley (A)",
+# "Willoughby (C)",
+# "Wollongong (C)"
+# ]
+
+long_p_rolling = long_p_rolling[list(lga_totals.lga)]
+
+long_p_rolling.index = long_p_rolling.index.strftime('%Y-%m-%d')
+
+long_stack = long_p_rolling.stack().reset_index().rename(columns={"level_1":"LGA", 0:"Trend in cases"})
+long_stack = long_stack.set_index('notification_date')	
+long_stack = long_stack.round(2)
+
+#%%
+
+def makeLgaTrend(df):
+	
+	template = [
+			{
+				"title": "Trend in cases by LGA in the Sydney Covid outbreak",
+				"subtitle": "Showing the seven-day rolling average in daily Covid cases for each LGA with five or more recent cases. Last updated {date}".format(date=newUpdated),
+				"footnote": "",
+				"source": " | Source: Guardian Australia analysis of NSW Health data",
+				"dateFormat": "%Y-%m-%d",
+				"xAxisLabel": "",
+				"yAxisLabel": "",
+				"timeInterval":"day",
+				"tooltip":"<b>{{#nicedate}}notification_date{{/nicedate}}</b><br><b>Cases, 7-day avg:</b> {{Trend in cases}}",
+				"periodDateFormat":"",
+				"margin-left": "27",
+				"margin-top": "5",
+				"margin-bottom": "22",
+				"margin-right": "22",
+				"xAxisDateFormat": "%b %d"
+			}
+		]
+	key = []
+	periods = []
+	labels = []
+	options = [{"numCols":4, "chartType":"line", "height":150, "scaleBy":"group"}]
+	chartId = [{"type":"smallmultiples"}]
+	df.fillna('', inplace=True)
+	df = df.reset_index()
+	chartData = df.to_dict('records')
+
+	yachtCharter(template=template,options=options, data=chartData, chartId=chartId, chartName="nsw-lga-trend-2021")
+
+makeLgaTrend(long_stack)
